4b8b0e7d397d265afb65de72dc43278cab7f6138
Pădureanu Andreea <padureanumaria22@stud.ase.ro>
2026-01-14 01:28:51 +0200

    hd

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 };
